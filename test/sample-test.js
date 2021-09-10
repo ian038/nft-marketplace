12f867027f19d1dbcaa6d4d2b074bc0a5f60d527
ian038 <ianphua3@gmail.com>
2021-09-10 16:47:34 -0400

    Updated test code

--- test/sample-test.js
+++ test/sample-test.js
@@ -27,7 +27,18 @@ describe("NftMarket", function () {
     const [_, buyerAddress] = await ethers.getSigners()
     await market.connect(buyerAddress).createMarketSale(nftAddress, 1, { value: auctionPrice })
 
-    const items = await market.fetchMarketItems()
+    let items = await market.fetchMarketItems()
+    items = await Promise.all(items.map(async i => {
+      const tokenUri = await nft.tokenURI(i.tokenId)
+      let item = {
+        price: i.price.toString(),
+        tokenId: i.tokenId.toString(),
+        seller: i.seller,
+        owner: i.owner,
+        tokenUri
+      }
+      return item
+    }))
     console.log('Items: ', items)
   });
 });
