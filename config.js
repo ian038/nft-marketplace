096a99ed6d9b0aa3d26ab27fcf280a7565fbfdf1
ian038 <ianphua3@gmail.com>
2021-09-17 19:17:26 -0400

    Fixing nextjs deployment issues

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 
-  export const nftmarketaddress = "0x7c2a9d96122D0f9FBe4ae513Ce70A4C95a1E1205"
-  export const nftaddress = "0x2d026D83caBeFEa25fe81622fCA8984a491F60C3"
+  export const nftmarketaddress = "0x2c2a01361079D1945d0384A99360b8E9383df5EF"
+  export const nftaddress = "0x95a5728eFe8b963a8d528440e5Ac07BCb6c4f9a9"
   
