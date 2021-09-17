58c143908c8105ed538ec8e0af2e46ddf2863dc3
ian038 <ianphua3@gmail.com>
2021-09-17 18:45:33 -0400

    Deployed to mumbai testnet

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 
-  export const nftmarketaddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
-  export const nftaddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
+  export const nftmarketaddress = "0xD78A941425134D2433Dc6a3D7EFde57089be8e84"
+  export const nftaddress = "0x7B838fF2BEC03d968fA126E495564864d0c85D48"
   
